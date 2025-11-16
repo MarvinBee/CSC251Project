f17f9d58bcafe5e287831a618481619ac6dc44f2
MarvinBee <mari8dot@gmail.com>
2025-11-16 18:05:56 -0500

    Modified project file
    Modified to read input from a file.

--- Project_marvin_barahona.java
+++ Project_marvin_barahona.java
@@ -1,14 +1,52 @@
 import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
+
 
 public class Project_marvin_barahona
 {
-   public static void main(String[] args)
+   public static void main(String[] args) throws IOException
    {
-      Scanner input = new Scanner(System.in);
+      
+      // creates the list of policy objects.
+      var policyObjList = new ArrayList<Policy>();
+      
+      // opens and processes the policyinformation.txt file
+      File policyInfo = new File("PolicyInformation.txt");
+      
+      Scanner inputFile = new Scanner(policyInfo); // "open"
+      
       int policyNum, policyholderAge;
       String providerName, policyholderFirstN, policyholderLastN, policyholderIsSmoking;
       double policyholderHeight, policyholderWeight;
       
+      // the loop that processes all the information of the file, and sets each list to its own object.
+      while(inputFile.hasNext())
+      {
+         
+         policyNum = inputFile.nextInt();
+         providerName = inputFile.nextLine();
+         policyholderFirstN = inputFile.nextLine();
+         policyholderLastN = inputFile.nextLine();
+         inputFile.nextLine(); // had problems here, maybe nextInt wasn't going down the line?
+         policyholderAge = inputFile.nextInt();
+         policyholderIsSmoking = inputFile.nextLine();
+         inputFile.nextLine(); // same as above
+         policyholderHeight = inputFile.nextDouble();
+         policyholderWeight = inputFile.nextDouble();
+         
+         // creates an object and adds it to the list.
+         policyObjList.add (new Policy(policyNum, providerName, policyholderFirstN, policyholderLastN, policyholderAge, policyholderIsSmoking, policyholderHeight, policyholderWeight));
+         
+         // checks if theirs more space
+         if(inputFile.hasNext())
+            inputFile.nextLine(); // clear the newline
+         if(inputFile.hasNext())
+            inputFile.nextLine(); // skip the blank
+                  
+      }
+      
+      /* old demo program (saved for future use)
       System.out.println("Please enter the Policy Number: ");
       policyNum = input.nextInt();
       input.nextLine();
@@ -50,7 +88,8 @@ public class Project_marvin_barahona
       System.out.printf("Policyholder's Weight: %.1f pounds\n", userPolicy.getPolicyholderWeight());
       System.out.printf("Policyholder's BMI: %.2f\n", userPolicy.getBMI());
       System.out.printf("Policy Price: $%.2f", userPolicy.getPolicyPrice(userPolicy.getBMI()));
-     
+      
+      */
       
    }
 }
